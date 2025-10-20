c54f6c0dff0b808c7c50116c18ef7717a0dcd56d
eduardo-joly <eduardo.niceinfo@gmail.com>
2025-10-19 21:38:51 -0300

    att

--- scripts.js
+++ scripts.js
@@ -35,7 +35,7 @@ function update(direction) {
     dots[active].classList.add('active');
 
     numberIndicator.textContent = string (active + 1).padStart(2, '0');
-    
+   
 }    
     
 clearInterval(timer);
